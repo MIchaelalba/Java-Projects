7c5ffa35fbbc01e22b0981dfcd01ced17ee71cbb
Michael Alba <33161280+MIchaelalba@users.noreply.github.com>
2023-04-07 17:15:59 -0300

    JAVA
    Conteudo em Java

---
+++ Java01_ConcatenarElementos/src/main/java/org/example/Concatenar_texto.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Concatenar_texto {
+    public static void concatenar(){
+        String nome = "Michael";
+        String sobrenome = "Alba";
+        int idade = 29;
+        double renda = 30000.0;
+        System.out.printf("%s %s tem %d anos e ganha R$ %.2f reais%n", nome, sobrenome, idade, renda);
+    }
+}
---
+++ Java01_ConcatenarElementos/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.example.Concatenar_texto.concatenar;
+
+public class Main {
+    public static void main(String[] args) {
+        concatenar();
+    }
+
+
+}
+
